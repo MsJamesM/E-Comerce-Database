dff6f8e863e4923a6994c339903dd338dfe67795
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-07-11 19:36:14 -0600

    completed tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,16 +17,45 @@ router.get("/:id", async (req, res) => {
   res.json(productTagData);
 });
 
-router.post("/", (req, res) => {
-  // create a new tag
+// creates a new tag
+router.post("/", async (req, res) => {
+  try {
+    const productTagData = await Tag.create(req.body);
+    res.status(200).json(productTagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+// updates a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedTag = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json({ message: "Tag updated successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+// deletes a tag by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const productTagData = await Tag.destroy({
+      include: [{ model: Product }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No tag with this id!" });
+      return;
+    }
+    res.status(200).json(productTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
